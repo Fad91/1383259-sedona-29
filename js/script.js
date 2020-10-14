4fd5e2abbfdc8231a3618647c9475b8be7a22e9a
Fad91 <homyak91@inbox.ru>
2020-10-14 21:34:11 +0400

    update

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ modalButton.addEventListener("click", function(evt) {
     evt.preventDefault();
     popup.classList.toggle("modal-show");
 });
+
+window.addEventListener("keydown", function(evt) {
+    if(evt.keyCode === 27) {
+    if(popup.classList.contains("modal-show")) {
+        evt.preventDefault();
+        popup.classList.remove("modal-show");
+    }
+    }
+});
